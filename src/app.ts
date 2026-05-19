cef56fec27ae173987d4826b4feafedf1e114af5
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-19 17:25:46 +0530

    feat: starting the server

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import express from "express";
+import connectDB from "./config/database";
+const app = express();
+const PORT:number = Number(process.env.PORT) || 7777;
+
+function startServer(port: number) {
+    const server = app.listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+    });
+
+    server.on('error', (err: any) => {
+        if(err.code === "EADDRINUSE") {
+            const nextPort = port + 1;
+            console.log(`Port ${port} is already in use, so trying on port ${port + 1}...`);
+            startServer(nextPort);
+        }
+        else {
+            console.error('Server error:', err);
+        }
+    });
+};
+
+connectDB()
+    .then(() => {
+        console.log('Database is connected successfully...');
+        startServer(PORT);
+    })
+    .catch((err) => {
+        console.error("Databsed connection failed : ", err)
+        process.exit(1);
+    });
+
