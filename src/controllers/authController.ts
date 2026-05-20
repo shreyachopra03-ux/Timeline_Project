5976a20ab3f6ac22b8e58f5fc0cf58adb1bf5809
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-20 19:26:02 +0530

    feat: add logic for hasing the password and create an instance of the user model

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,28 +7,36 @@ export const registerUser = async (req: Request, res: Response) => {
 
     try {
         const errors = validationResult(req);
-        console.log(errors);
+        // console.log(errors);
         if(!errors.isEmpty()) {
-            res.status(400).json({ errors: errors.array() });
+            res.status(400).json({ error: errors.array()[0].msg });
             return;
         }
 
-    const { name, email, password } = req.body;
+    const { name, email, password, avatar } = req.body;
 
     const existingUser = await User.findOne({ email });
     if(existingUser) {
         res.status(400).json({ message: "Email already registered" });
         return;
     }
+    // Hash the password before saving it into the DB
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
 
-    res.status(200).json({
-        data: { name, email },
-        message: "Success"
+    // Create a new instance of the User model
+    const user = new User({
+        name,
+        email,
+        password: hashedPassword,
+        avatar
     });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-
+    await user.save();
+    res.status(200).json({ 
+        data: { name, email },
+        message: "User registered successfully!" 
+    });
     } 
     catch (err: any) {
         res.status(500).json({ message: "Server error", err})
