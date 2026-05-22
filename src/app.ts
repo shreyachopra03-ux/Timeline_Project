06c03833647935c88b1742a7f02f319635a5adf3
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-22 19:50:41 +0530

    feat: add clerk middleware and webhook route

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,16 @@
-import "dotenv/config";
 import express from "express";
-import connectDB from "./config/database";
+import { clerkMiddleware } from "@clerk/express";
 const app = express();
-const PORT:number = Number(process.env.PORT) || 7777;
-import authRouter from "./routes/auth";
+import webhookRouter from "./routes/webhookRoutes";
+
+
+// Middleware
+app.use(clerkMiddleware());
+
+// Routes
+app.use("/api/webhooks", express.raw({ type: "application/json" }), webhookRouter);
+
 
 app.use(express.json());
 
-// Routes
-app.use("/", authRouter);
-
-function startServer(port: number) {
-    const server = app.listen(port, () => {
-        console.log(`Server is listening on port ${port}`);
-    });
-
-    server.on('error', (err: any) => {
-        if(err.code === "EADDRINUSE") {
-            const nextPort = port + 1;
-            console.log(`Port ${port} is already in use, so trying on port ${port + 1}...`);
-            startServer(nextPort);
-        }
-        else {
-            console.error('Server error:', err);
-        }
-    });
-};
-
-connectDB()
-    .then(() => {
-        console.log('Database is connected successfully...');
-        startServer(PORT);
-    })
-    .catch((err) => {
-        console.error("Databsed connection failed : ", err)
-        process.exit(1);
-    });
 
