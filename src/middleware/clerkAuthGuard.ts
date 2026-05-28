3b82828a86ac266685ca16aea669022becf3eefd
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-28 15:37:09 +0530

    feat: add clerk authentication middleware

---
+++ src/middleware/clerkAuthGuard.ts
@@ -0,0 +1,23 @@
+import { getAuth } from "@clerk/express";
+import type { Request, Response, NextFunction } from "express";
+
+interface CustomAuthRequest extends Request {
+  user?: {
+    id: string;
+  };
+}
+
+// Middleware to check whether the user is signed in or not.
+export const clerkAuthGuard = (req: CustomAuthRequest, res: Response, next: NextFunction) => {
+  const { userId } = getAuth(req);
+  
+  if (!userId) {
+    return res.status(401).json({ 
+      success: false, 
+      message: "Unauthorized: Please sign in to upload photos." 
+    });
+  }
+  
+  req.user = { id: userId }; 
+  next();
+};
