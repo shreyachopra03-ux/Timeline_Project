7ca7de0cff6db51d4b49964ca871dbd57c3e56c7
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-29 00:49:19 +0530

    feat: make route for uploading bulk photos

--- src/routes/photoRoute.ts
+++ src/routes/photoRoute.ts
@@ -2,8 +2,11 @@ import express from "express";
 import { uploadPhoto } from "../controllers/photoController";
 import { upload } from "../middleware/multer";
 import { clerkAuthGuard } from "../middleware/clerkAuthGuard";
+import { uploadBulkPhotos } from "../controllers/photoController";
 const photoRouter = express.Router();
 
 photoRouter.post("/upload", clerkAuthGuard, upload.single("photo"), uploadPhoto as any);
+
+photoRouter.post("/upload-bulk", clerkAuthGuard, upload.array("photos"), uploadBulkPhotos as any);
 
 export default photoRouter;
