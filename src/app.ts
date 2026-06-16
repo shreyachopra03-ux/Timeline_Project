33f13fd964b9dc6b4d460c18fa581fce6dbf4a42
Shreya Chopra <chopra.shreya03@gmail.com>
2026-06-16 17:27:05 +0530

    refactor: minor code cleanup

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,10 @@ app.use((req, res, next) => {
 app.use(clerkMiddleware());
 
 app.use("/api/webhooks", express.raw({ type: "application/json" }), webhookRouter);
-app.use(express.json());
+
+app.use(express.json({ limit: "100mb" })); 
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
 app.use("/media", mediaRouter);
 
 function startServer(port: number) {
