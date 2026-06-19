5ec85f67455acc1e02aa0fa0c0f9bd87675a86da
Shreya Chopra <chopra.shreya03@gmail.com>
2026-06-19 16:28:52 +0530

    refactor: minor code cleanup

--- src/routes/webhookRoutes.ts
+++ src/routes/webhookRoutes.ts
@@ -2,6 +2,6 @@ import express from "express";
 const webhookRouter = express.Router(); 
 import { handleClerkWebhook } from "../controllers/webhookController";
 
-webhookRouter.post("/clerk", handleClerkWebhook);
+webhookRouter.post("/", handleClerkWebhook);
 
 export default webhookRouter;
