07e61fc9a97a73cf03757c777f4ebc9f0f7405fa
Shreya Chopra <chopra.shreya03@gmail.com>
2026-06-19 16:32:28 +0530

    refactor: minor code cleanup

--- src/middleware/clerkAuthGuard.ts
+++ src/middleware/clerkAuthGuard.ts
@@ -2,22 +2,21 @@ import { getAuth } from "@clerk/express";
 import type { Request, Response, NextFunction } from "express";
 
 interface CustomAuthRequest extends Request {
-  user?: {
-    id: string;
-  };
+    user?: {
+        id: string;
+    };
 }
 
-// Middleware to check whether the user is signed in or not.
 export const clerkAuthGuard = (req: CustomAuthRequest, res: Response, next: NextFunction) => {
-  const { userId } = getAuth(req);
-  
-  if (!userId) {
-    return res.status(401).json({ 
-      success: false, 
-      message: "Unauthorized: Please sign in to upload photos." 
-    });
-  }
-  
-  req.user = { id: userId }; 
-  next();
+    const { userId } = getAuth(req);
+
+    if (!userId) {
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized: Please sign in to upload photos."
+        });
+    }
+
+    req.user = { id: userId };
+    next();
 };
