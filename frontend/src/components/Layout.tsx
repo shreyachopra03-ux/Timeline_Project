94af13100e3793dbad3745eba30275575b3a4f3e
Shreya Chopra <chopra.shreya03@gmail.com>
2026-06-22 21:20:29 +0530

    feat: create shared layout structure

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,16 @@
 import { ReactNode } from 'react'
+import Sidebar from './Sidebar'
+import MobileNav from './MobileNav'
 import FilmstripFooter from './FilmstripFooter'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen bg-background pb-20">
-      {children}
+    <div className="min-h-screen bg-background">
+      <Sidebar />
+      <main className="lg:ml-60 pb-20 lg:pb-0">
+        {children}
+      </main>
+      <MobileNav />
       <FilmstripFooter years={[]} />
     </div>
-  )
-}
+  )};
